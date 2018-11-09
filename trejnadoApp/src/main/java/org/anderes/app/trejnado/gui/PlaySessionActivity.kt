09f8ee40d5ef4fc7ba969ed917472b95466f9d85
René Anderes <randeres@yahoo.de>
2018-11-09 12:08:30 +0100

    Trainingssession

--- trejnadoApp/src/main/java/org/anderes/app/trejnado/gui/PlaySessionActivity.kt
+++ trejnadoApp/src/main/java/org/anderes/app/trejnado/gui/PlaySessionActivity.kt
@@ -1,25 +1,100 @@
 package org.anderes.app.trejnado.gui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.widget.ImageButton
 
 import kotlinx.android.synthetic.main.activity_play_session.*
 import org.anderes.app.trejnado.R
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_program.*
+import kotlinx.android.synthetic.main.content_play_session.*
+import org.anderes.app.trejnado.Constants
+import org.anderes.app.trejnado.TrainingProgram
+
 
 class PlaySessionActivity : AppCompatActivity() {
+
+    var running: Boolean = false
+    var startTime: Long = 0
+    lateinit var trainingProgram: TrainingProgram
+    var programUnitNo: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_session)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(play_session_toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val context = view.context
+            val intent = Intent(context, PlaySessionActivity::class.java)
+            intent.putExtra(Constants.PARAM_PROGRAM_ID, trainingProgram.key)
+            intent.putExtra(Constants.PARAM_PROGRAM_UNIT_NO, programUnitNo + 1)
+            context.startActivity(intent)
         }
 
         // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val programId = intent.getStringExtra(Constants.PARAM_PROGRAM_ID)
+        programUnitNo = intent.getIntExtra(Constants.PARAM_PROGRAM_UNIT_NO, 0)
+        val databaseRef = FirebaseDatabase.getInstance().reference
+        databaseRef
+            .child(Constants.TRAINING_PROGRAM_CHILD)
+            .child(programId).addListenerForSingleValueEvent( object: ValueEventListener {
+                override fun onCancelled(databaseError: DatabaseError) {
+                    play_session_toolbar.title = "Error"
+                }
+
+                override fun onDataChange(snpashot: DataSnapshot) {
+                    trainingProgram = snpashot.getValue(TrainingProgram::class.java)!!
+                    play_session_toolbar.title = trainingProgram.name
+                    val settinglistView: RecyclerView = findViewById(R.id.machine_settings_view_id)
+                    settinglistView.adapter = SettinglistAdapter(trainingProgram.machines.get(programUnitNo).settings)
+                    play_session_machine_id.text = trainingProgram.machines.get(programUnitNo).name
+                }
+
+            })
+
+        val stopwatchButton: ImageButton = findViewById(R.id.play_session_stopwatch_button)
+        stopwatchButton.setOnClickListener { view ->
+
+            if (!running) {
+                startTime = SystemClock.uptimeMillis()
+                runTimer()
+                stopwatchButton.setImageResource(android.R.drawable.ic_media_pause)
+            } else {
+                stopwatchButton.setImageResource(android.R.drawable.ic_media_play)
+            }
+            running = !running
+        }
+    }
+
+    private fun runTimer() {
+        val textView = findViewById(R.id.play_session_edit_time) as TextView
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val millisecondTime = SystemClock.uptimeMillis() - startTime;
+                var seconds = millisecondTime / 1000
+                seconds = seconds % 60;
+                textView.text = String.format("%d", seconds)
+                if (running) {
+                    handler.postDelayed(this, 100)
+                } else {
+                    handler.removeCallbacks(this)
+                }
+
+            }
+        })
+
     }
 
 }
--- trejnadoApp/src/main/java/org/anderes/app/trejnado/gui/ProgramActivity.kt
+++ trejnadoApp/src/main/java/org/anderes/app/trejnado/gui/ProgramActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.DataSnapshot
@@ -15,10 +16,12 @@ import kotlinx.android.synthetic.main.activity_program.*
 import org.anderes.app.trejnado.Constants
 import org.anderes.app.trejnado.R
 import org.anderes.app.trejnado.TrainingMachine
+import org.anderes.app.trejnado.TrainingProgram
 
 class ProgramActivity : AppCompatActivity() {
 
     lateinit var adapter: MachinelistAdapter
+    lateinit var programId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +31,19 @@ class ProgramActivity : AppCompatActivity() {
         playSessionActionButton.setOnClickListener { view ->
             val context = view.context
             val intent = Intent(context, PlaySessionActivity::class.java)
+            intent.putExtra(Constants.PARAM_PROGRAM_ID, programId)
+            intent.putExtra(Constants.PARAM_PROGRAM_UNIT_NO, 0)
             context.startActivity(intent)
         }
 
         // Show the Up button in the action bar.
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val programId = intent.getStringExtra(Constants.PARAM_PROGRAM_ID)
+        programId = intent.getStringExtra(Constants.PARAM_PROGRAM_ID)
         val machinelistRecyclerView = findViewById<View>(R.id.machine_list_id) as RecyclerView
         val mLinearLayoutManager = LinearLayoutManager(this)
-        val databseRef = FirebaseDatabase.getInstance().reference
-        val machinesRef = databseRef.child(Constants.TRAINING_PROGRAM_CHILD)
+        val databaseRef = FirebaseDatabase.getInstance().reference
+        val machinesRef = databaseRef.child(Constants.TRAINING_PROGRAM_CHILD)
                                     .child(programId)
                                     .child(Constants.TRAINING_PROGRAM_MACHINE_CHILD)
                                     .orderByChild(Constants.TRAINING_PROGRAM_MACHINE_SEQ_CHILD)
@@ -49,17 +54,19 @@ class ProgramActivity : AppCompatActivity() {
         machinelistRecyclerView.layoutManager = mLinearLayoutManager
         machinelistRecyclerView.adapter = adapter
 
-        databseRef
+        databaseRef
             .child(Constants.TRAINING_PROGRAM_CHILD)
-            .child(programId).child(Constants.TRAINING_PROGRAM_NAME_CHILD).addListenerForSingleValueEvent( object: ValueEventListener {
-                override fun onCancelled(databaseError: DatabaseError) {
-                    program_toolbar.title = "Error"
-                }
-
+            .child(programId).addListenerForSingleValueEvent( object: ValueEventListener {
                 override fun onDataChange(snpashot: DataSnapshot) {
-                    program_toolbar.title = snpashot.value as String
+                    Log.d("GUI", "Key: " + snpashot.key)
+                    val trainingProgram = snpashot.getValue(TrainingProgram::class.java)
+                    program_toolbar.title = trainingProgram!!.name
                 }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    program_toolbar.title = "Error: " + databaseError.message
+                    Log.w("GUI", databaseError.toException());
 
+                }
             })
     }
 
